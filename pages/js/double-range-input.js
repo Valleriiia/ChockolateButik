03a382794b3bbd9f866cff0d220ef395c0b4a026
Valleriiia <valeria.nepomiashcha@gmail.com>
2025-03-25 17:12:04 +0200

    edits

--- pages/js/double-range-input.js
+++ pages/js/double-range-input.js
@@ -2,7 +2,14 @@ const minRange = document.getElementById("min-range");
 const maxRange = document.getElementById("max-range");
 const minInput = document.getElementById("min-input");
 const maxInput = document.getElementById("max-input");
-const track = document.querySelector(".slider-track");
+const track = document.querySelector(".slider-track-main");
+
+const minRangeSide = document.getElementById("min-range-side");
+const maxRangeSide = document.getElementById("max-range-side");
+const minInputSide = document.getElementById("min-input-side");
+const maxInputSide = document.getElementById("max-input-side");
+const trackSide = document.querySelector(".slider-track-side");
+
 const rangeMin = 0;
 const rangeMax = 2000;
 
@@ -23,10 +30,26 @@ function updateTrack() {
   track.style.width = (percent2 - percent1) + "%";
 }
 
+function updateTrackSide() {
+  if (!trackSide) return;
+  const min = parseInt(minRangeSide.value);
+  const max = parseInt(maxRangeSide.value);
+  const percent1 = ((min - rangeMin) / (rangeMax - rangeMin)) * 100;
+  const percent2 = ((max - rangeMin) / (rangeMax - rangeMin)) * 100;
+  trackSide.style.left = percent1 + "%";
+  trackSide.style.width = (percent2 - percent1) + "%";
+}
+
 function syncInputsFromSlider() {
   minInput.value = formatCurrency(minRange.value);
   maxInput.value = formatCurrency(maxRange.value);
   updateTrack();
+}
+
+function syncInputsFromSliderSide() {
+  minInputSide.value = formatCurrency(minRangeSide.value);
+  maxInputSide.value = formatCurrency(maxRangeSide.value);
+  updateTrackSide();
 }
 
 function syncSlidersFromInput() {
@@ -42,6 +65,21 @@ function syncSlidersFromInput() {
   maxInput.value = formatCurrency(max);
 
   updateTrack();
+}
+
+function syncSlidersFromInputSide() {
+  let min = parseCurrency(minInputSide.value);
+  let max = parseCurrency(maxInputSide.value);
+  if (min > max) min = max;
+  if (max < min) max = min;
+
+  minRangeSide.value = min;
+  maxRangeSide.value = max;
+
+  minInputSide.value = formatCurrency(min);
+  maxInputSide.value = formatCurrency(max);
+
+  updateTrackSide();
 }
 
 minRange.addEventListener("input", () => {
@@ -72,5 +110,36 @@ maxInput.addEventListener("input", () => {
   syncSlidersFromInput();
 });
 
+minRangeSide.addEventListener("input", () => {
+  if (+minRangeSide.value > +maxRangeSide.value) {
+    minRangeSide.value = maxRangeSide.value;
+  }
+  syncInputsFromSliderSide();
+});
+
+maxRangeSide.addEventListener("input", () => {
+  if (+maxRangeSide.value < +minRangeSide.value) {
+    maxRangeSide.value = minRangeSide.value;
+  }
+  syncInputsFromSliderSide();
+});
+
+minInputSide.addEventListener("input", () => {
+  if (parseCurrency(minInputSide.value) > parseCurrency(maxInputSide.value)) {
+    minInputSide.value = maxInputSide.value;
+  }
+  syncSlidersFromInputSide();
+});
+
+maxInputSide.addEventListener("input", () => {
+  if (parseCurrency(maxInputSide.value) < parseCurrency(minInputSide.value)) {
+    maxInputSide.value = minInputSide.value;
+  }
+  syncSlidersFromInputSide();
+});
+
 updateTrack(); 
 syncInputsFromSlider(); 
+
+updateTrackSide(); 
+syncInputsFromSliderSide(); 
